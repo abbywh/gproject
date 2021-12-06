edf1d79188cac0fa9da302d8394ccf8417b7a71c
Joel Whittier <jdw23@uchicago.edu>
2021-12-06 12:49:54 -0600

    randomizer working need to format image

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,24 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "graceProject",
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-styled-components",
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+  ],
 };
